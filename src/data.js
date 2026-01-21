71d5957733e01af4f7da69472841a5aaf1d4d9af
Syed <sygaki786@gmail.com>
2026-01-20 21:47:56 -0500

    Tours component

---
+++ src/data.js
@@ -0,0 +1,93 @@
+import tour1 from "./images/tour-1.jpeg"
+import tour2 from "./images/tour-2.jpeg"
+import tour3 from "./images/tour-3.jpeg"
+import tour4 from "./images/tour-4.jpeg"
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+]
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://www.twitter.com",
+    icon: "fab fa-facebook",
+  },
+  {
+    id: 2,
+    href: "https://www.twitter.com",
+    icon: "fab fa-twitter",
+  },
+  {
+    id: 3,
+    href: "https://www.twitter.com",
+    icon: "fab fa-squarespace",
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "Saving Money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "Endless Hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "Amazing Comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "August 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "China",
+    duration: 6,
+    price: "From $2100",
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "August 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "China",
+    duration: 6,
+    price: "From $2100",
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "August 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "China",
+    duration: 6,
+    price: "From $2100",
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "August 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "China",
+    duration: 6,
+    price: "$2100",
+  },
+]
